36f4171d6bc59ccc63364d906bd990f685842a31
Adam B <atomicbrod101@gmail.com>
2017-02-05 19:03:17 -0500

    Improved reddit command

--- cmds/reddit.py
+++ cmds/reddit.py
@@ -1,4 +1,4 @@
-import paw
+import aiohttp
 
 
 # Reddit command
@@ -6,17 +6,20 @@ async def ex(c, pch, dch, m, a, CMD_CHAR):
     a = a.split(' ')
     if len(a) > 0:
         a = a[0].lower()
-        try:
-            re = paw.Reddit()
-            r = '''20 Hot Submissions from Subreddit: `{}`'''.format(a)
-            for p in re.subreddit(a).hot(limit=20):
-                r += '''<{}>'''.format(p.shortlink)
-            await c.send_message(pch, '{}'.format(r))
-            await c.send_message(dch, '{}, I sent you a list of links to different submissions in `{}` subreddit in a '
-                                      'private message!'.format(m, a))
-        except Exception as exc:
-            await c.send_message(dch, '{}, unable to find the submissions from the subreddit `{}`!'.format(m, a))
-            print(exc)
-
+        async with aiohttp.ClientSession() as s:
+            async with s.get('https://www.reddit.com/r/{}/.json'.format(a)) as r:
+                d = await r.json()
+                if d['message'] is not 'Forbidden':
+                    r = '''20 Hot Submissions from Subreddit: `{}`'''.format(a)
+                    co = 0
+                    for i in d['data']['children']:
+                        if i['selftext_html'] is None:
+                            co += 1
+                            r += '''<{}>'''.format('https://www.reddit.com' + i['selftext_html'])
+                        if co >= 20:
+                            break
+                else:
+                    await c.send_message(dch, '{}, unable to get submissions from subreddit `{}` because it doesnt '
+                                              'exist!'.format(m, a))
     else:
         await c.send_message(dch, '{}, **USAGE:** {}reddit <subreddit>'.format(m, CMD_CHAR))
