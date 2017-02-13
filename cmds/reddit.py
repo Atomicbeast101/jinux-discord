a60cecf5459fc2daf87d69b042def0860881931a
Adam Bro <atomicbrod101@gmail.com>
2017-02-13 15:31:01 -0500

    Crushing bug...

--- cmds/reddit.py
+++ cmds/reddit.py
@@ -4,22 +4,24 @@ import aiohttp
 # Reddit command
 async def ex(c, pch, dch, m, a):
     a = a.split(' ')
-    if len(a) == 0:
+    if len(a) == 1:
         async with aiohttp.ClientSession() as s:
             async with s.get('https://reddit.com/.json') as r:
                 d = await r.json()
                 r = '''Hot 10 Submissions in Reddit's Front Page:
 '''
                 cnt = 1
-                for s in d['data']['children']:
+                for sr in d['data']['children']:
+                    title = sr['data']['title']
+                    url = sr['data']['permalink']
                     r += '''{}) {}
-    {}
-'''.format(cnt, s['data']['secure_media']['oembed']['title'])
+    <https://reddit.com{}>
+'''.format(cnt, title, url)
                     cnt += 1
                 await c.send_message(pch, r)
                 await c.send_message(dch, '{}, I sent the list in a private message.'.format(m))
     else:
-        a = a[0]
+        a = a[1]
         async with aiohttp.ClientSession() as s:
             async with s.get('https://reddit.com/r/{}/.json'.format(a)) as r:
                 d = await r.json()
@@ -27,10 +29,12 @@ async def ex(c, pch, dch, m, a):
 '''.format(a)
                 cnt = 1
                 for sr in d['data']['children']:
-                    if sr['data']['domain'] != 'self.movies':
+                    if not sr['data']['author'] != 'AutoModerator':
+                        title = sr['data']['title']
+                        url = sr['data']['permalink']
                         r += '''{}) {}
-    {}
-'''.format(cnt, sr['data']['secure_media']['oembed']['title'])
+    <https://reddit.com{}>
+'''.format(cnt, title, url)
                     cnt += 1
                 await c.send_message(pch, r)
                 await c.send_message(dch, '{}, I sent the list in a private message.'.format(m))
