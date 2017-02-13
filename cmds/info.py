ac8751ae56311362507c4f8e82267b4ec1705105
Adam Bro <atomicbrod101@gmail.com>
2017-02-12 20:24:50 -0500

    Updated repository

--- cmds/info.py
+++ cmds/info.py
@@ -1,4 +1,4 @@
 # Info command
 async def ex(c, ch):
     await c.send_message(ch, "I'm a bot, obviously. My creator is Atomicbeast101 and you can find my source files at "
-                             "http://github.com/Atomicbeast101/Discord-JProject")
+                             "http://github.com/Atomicbeast101/Jinux")
