d4619b207533d9133f78c309ec891e7f31a3bd8f
Adam Bro <atomicbrod101@gmail.com>
2017-03-06 14:32:53 -0500

    Improved user friendly.

--- cmds/time.py
+++ cmds/time.py
@@ -5,17 +5,16 @@ from geopy import geocoders
 # Time command
 async def ex(dclient, channel, mention, a, cmd_char):
     if len(a) > 0:
-        if len(a) == 3:
-            a = a.upper()
-            g = geocoders.GoogleV3()
-            try:
-                place, (lat, lng) = g.geocode(a)
-                timezone = g.timezone((lat, lng))
-                time_now = datetime.now(timezone)
-                await dclient.send_message(channel, 'It is `{:%H:%M:%S}` or `{:%I:%M:%S %p}` in `{}` right now.'
-                                           .format(time_now, time_now, a))
-            except Exception:
-                await dclient.send_message(channel, 'Invalid timezone input, `{}`! Please check <https://en.wikipedia.'
-                                                    'org/wiki/List_of_tz_database_time_zones>, {}!'.format(a, mention))
+        g = geocoders.GoogleV3()
+        try:
+            place, (lat, lng) = g.geocode(a)
+            timezone = g.timezone((lat, lng))
+            time_now = datetime.now(timezone)
+            await dclient.send_message(channel, 'It is `{:%H:%M:%S}` or `{:%I:%M:%S %p}` in `{}` right now.'
+                                       .format(time_now, time_now, a))
+        except Exception:
+            await dclient.send_message(channel, 'Location `{}` unknown! Please try to be more specific! I rely on '
+                                                'Google maps to get the coordinates for the timezone, {}!'
+                                       .format(a, mention))
     else:
         await dclient.send_message(channel, '{}, **USAGE:** {}time <timezone>'.format(mention, cmd_char))
