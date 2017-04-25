b998bc127eb54b6f03765345ba6dc1079c39daf0
Adam Bro <atomicbrod101@gmail.com>
2017-04-24 22:35:32 -0400

    Added ability to retrieve specific XKCD comic ID #

--- cmds/xkcd.py
+++ cmds/xkcd.py
@@ -4,8 +4,16 @@ import requests
 import aiohttp
 
 
+def is_int(a):
+    try:
+        int(a)
+        return True
+    except ValueError:
+        return False
+
+
 # xkcd command
-async def ex(dclient, channel, a):
+async def ex(dclient, channel, mention, a, cmd_char):
     if len(a) > 0:
         a = a.split(' ')[0]
         if a == 'latest':
@@ -13,17 +21,23 @@ async def ex(dclient, channel, a):
                 async with s.get('https://xkcd.com/info.0.json') as r:
                     d = await r.json()
                     await dclient.send_message(channel, '''Title: {}
-{}'''.format(d['safe_title'], d['img']))
+Comic ID: {}
+{}'''.format(d['safe_title'], d['num'], d['img']))
+        elif is_int(a):
+            comic_id = a
+            try:
+                async with aiohttp.ClientSession() as s:
+                    async with s.get('https://xkcd.com/{}/info.0.json'.format(comic_id)) as r:
+                        d = await r.json()
+                        await dclient.send_message(channel, '''Title: {}
+Comic ID: {}
+{}'''.format(d['safe_title'], d['num'], d['img']))
+            except Exception:
+                await dclient.send_message(channel, "{}, unable to retrieve a specific comic! Perhaps it doesn't "
+                                                    "exist?".format(mention, cmd_char))
         else:
-            b = BeautifulSoup(requests.get('https://xkcd.com/archive/').text, 'html.parser')
-            ids = b.find('div', id='middleContainer').find_all('a')
-            idr = choice(ids)['href']
-            idr.replace('/', '')
-            async with aiohttp.ClientSession() as s:
-                async with s.get('https://xkcd.com/{}/info.0.json'.format(idr)) as r:
-                    d = await r.json()
-                    await dclient.send_message(channel, '''Title: {}
-{}'''.format(d['safe_title'], d['img']))
+            await dclient.send_message(channel,
+                                       '{}, **USAGE:** {}xkcd <latest OR comic_ID>'.format(mention, cmd_char))
     else:
         b = BeautifulSoup(requests.get('https://xkcd.com/archive/').text, 'html.parser')
         ids = b.find('div', id='middleContainer').find_all('a')
@@ -33,4 +47,5 @@ async def ex(dclient, channel, a):
             async with s.get('https://xkcd.com/{}/info.0.json'.format(idr)) as r:
                 d = await r.json()
                 await dclient.send_message(channel, '''Title: {}
-{}'''.format(d['safe_title'], d['img']))
+Comic ID: {}
+{}'''.format(d['safe_title'], d['num'], d['img']))
