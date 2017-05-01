559a37676c470538ca9e71eef74b5f014b89c5b4
Adam Bro <atomicbrod101@gmail.com>
2017-05-01 12:46:55 -0400

    Added code block to make it pretty for -conspiracy

--- cmds/conspiracy.py
+++ cmds/conspiracy.py
@@ -2,4 +2,4 @@ import random as r
 
 # Conspiracy command
 async def ex(dclient, channel, conspiracy_list):
-    await dclient.send_message(channel, '{}'.format(r.choice(conspiracy_list)))
+    await dclient.send_message(channel, '```{}```'.format(r.choice(conspiracy_list)))
